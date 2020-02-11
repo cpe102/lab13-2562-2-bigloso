560d4e92a86e1c51c3ffa9ff019699f1f1aa19df
Bigloso <wayside.big@gmail.com>
2020-02-11 17:44:03 +0700

    123

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix( double A[][N]);
+void inputMatrix(double [][N]);
 
-void findLocalMax(const double A[][N], bool B[][N]);
+void findLocalMax(const double [][N], bool [][N]);
 
-void showMatrix(const bool B[][N]);
+void showMatrix(const bool [][N]);
 
 int main(){
 	double A[N][N]; 
@@ -33,26 +33,27 @@ void inputMatrix(double A[][N]){
 }
 
 void findLocalMax(const double A[][N], bool B[][N]){
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			B[i][j] = 0 ;
+	for(int a=0;a<N;a++){
+		for(int b=0;b<N;b++)
+		B[a][b]=0;
+	}
+	for(int i=1;i<N-1;i++){
+		for(int j=1;j<N-1;j++){
+			if(A[i][j]>=A[i-1][j])
+			if(A[i][j]>=A[i][j-1])
+			if(A[i][j]>=A[i][j+1])
+			if(A[i][j]>=A[i+1][j])
+			B[i][j]=1;
+			else B[i][j]=0;
 		}
 	}
-
-	for(int i=1 ;i<N-1;i++){
-		for(int j=1;j=N-1;j++){
-			if(A[i][j]>=A[i+1][j] && A[i][j]>=A[i-1][j] && A[i][j]>=A[i][j+1] && A[i][j]>=A[i][j-1] ){
-				B[i][j] = 1 ;
-			}
-			else B[i][j] = 0 ;
-		}
-	}	
+	
 }
 
 void showMatrix(const bool B[][N]){
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			cout << B[i][j] << " " ;
-		}
+	for(int i =0;i<N;i++){
+		for(int j =0;j<N;j++) cout<<B[i][j]<<" ";
+		cout <<"\n";
 	}
+
 }
